9f830d01273eb3bac87f65c154e50997afd41def
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-11-17 16:13:58 +0530

    roboshop-ansible

--- launch-instances-script/create-ec2.sh
+++ launch-instances-script/create-ec2.sh
@@ -6,21 +6,25 @@ SG_NAME="MSG-ZEESHAN"
 IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::085894364751:instance-profile/role-for-secret-manger-readonly-roboshop-components"
 #############################
 
-
+if [ -z "${1}" ] ; then
+  ENV=""
+else
+  ENV="-$1"
+fi
 COMPONENT=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}${ENV}}, {Key=Monitor,Value=yes}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
-      --iam-instance-profile="${IAM_INSTANCE_PROFILE}"
+      --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}${ENV}/" route53.json >/tmp/record.json
-    aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
-  }
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 AMI_ID="ami-0b618b7be3534c792"
